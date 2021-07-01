c4dcee476db9360eb570aaf0874ffa4981c5ca07
altruios <49083558+altruios@users.noreply.github.com>
2021-07-01 16:17:02 -0700

    spite updates - and testing code ideas

--- Steve.gd
+++ Steve.gd
@@ -15,5 +15,7 @@ func _physics_process(delta):
 		velocity.x=speed
 	if(Input.is_action_pressed("jump") and is_on_floor()):
 		velocity.y=jump_force
+
+		
 	move_and_slide(velocity, Vector2.UP)
 	velocity.x = lerp(velocity.x,0,0.1)
