65575f336e4d37aa7044e3dadbae784e9f61f4bc
Shaw Summa <shawsumma@gmail.com>
2021-07-02 04:53:45 -0400

    swarm movement, try keys 1 2 3 (debug)

--- Steve.gd
+++ Steve.gd
@@ -1,20 +1,18 @@
 extends KinematicBody2D
 
 var velocity = Vector2(0,0)
-var speed=210
+var speed=21
 var gravity = 30
-var jump_force = -800
+var jump_force = -350
+
 func _physics_process(delta):
 	velocity.y=velocity.y+gravity
-	if(Input.is_action_pressed("left")):
-		velocity.x=-speed
-	if(Input.is_action_pressed("right")):
-		velocity.x=speed
-	if(Input.is_action_pressed("jump") and is_on_floor()):
+	if (Input.is_action_pressed("left")):
+		velocity.x-=speed
+	if (Input.is_action_pressed("right")):
+		velocity.x+=speed
+	if (is_on_floor()):
 		velocity.y=jump_force
-	elif is_on_floor():
-		velocity.y=0;
-
-		
+	
 	move_and_slide(velocity, Vector2.UP)
-	velocity.x = lerp(velocity.x,0,0.1)
+	velocity.x *= 0.95
